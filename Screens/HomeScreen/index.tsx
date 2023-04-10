1262a875492df6fd25cd513f55befa764760cfad
shaikh99mubashir <shaikh99mubashir@gmail.com>
2023-04-10 15:25:45 +0500

    Update

--- Screens/HomeScreen/index.tsx
+++ Screens/HomeScreen/index.tsx
@@ -3,19 +3,56 @@ import {
   Text,
   TouchableOpacity,
   View,
+  FlatList,
   TextInput,
+  Dimensions,
+  Image,
 } from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
 import Header from '../../Components/Header';
 import {Color} from '../../Constants';
 
 const HomeScreen = ({navigation}: any) => {
+  const HomePageBanner = [
+    {
+      id: 1,
+      name: 'asd',
+      image: require('../../Images/HomePageBanner.png'),
+    },
+    {
+      id: 2,
+      name: 'qqq',
+      image: require('../../Images/HomePageBanner.png'),
+    },
+    {
+      id: 3,
+      name: 'www',
+      image: require('../../Images/HomePageBanner.png'),
+    },
+  ];
+  const renderHomePageBanner = ({item}: any): any => {
+    console.log('item==>', item);
+    return (
+      <View
+        style={{
+          marginTop: 15,
+          alignItems: 'center',
+          paddingHorizontal: 20,
+        }}>
+        <Image
+          source={item.image}
+          resizeMode="cover"
+          style={{width: Dimensions.get('window').width / 1.2, height: 150}}
+        />
+      </View>
+    );
+  };
   return (
     <View style={{backgroundColor: Color.mainColor, paddingHorizontal: 10}}>
       <Header navigation={navigation} Drawer={true} Notification />
       {/* Search */}
-
       <View
         style={{
           width: '98%',
@@ -30,7 +67,7 @@ const HomeScreen = ({navigation}: any) => {
           paddingHorizontal: 10,
           alignSelf: 'center',
           backgroundColor: Color.white,
-          marginTop: 10,
+          marginTop: 15,
         }}>
         <TextInput
           placeholder="Search"
@@ -48,8 +85,15 @@ const HomeScreen = ({navigation}: any) => {
         </TouchableOpacity>
       </View>
 
-      <Text style={{fontFamily: 'Poppins-SemiBold'}}>HomeScreen</Text>
-      <Icon name="ios-calendar-sharp" size={22} />
+      <FlatList
+        data={HomePageBanner}
+        renderItem={renderHomePageBanner}
+        keyExtractor={(item: any) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+
+      <Entypo name="dot-single" size={22} />
     </View>
   );
 };
