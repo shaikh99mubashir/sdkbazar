c434edf3c7c295a727f6c408c914c8a02bc3b5da
shaikh99mubashir <shaikh99mubashir@gmail.com>
2023-04-11 14:36:51 +0500

    update

--- RoutingConfig/MyTabs.tsx
+++ RoutingConfig/MyTabs.tsx
@@ -1,20 +1,225 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../Screens/HomeScreen';
 import Splash from '../Screens/Splash';
+import History from '../Screens/History';
+import Feedback from '../Screens/Feedback';
+import LoginUserProfile from '../Screens/LoginUserProfile';
+import {Color} from '../Constants';
 
 const Tab = createBottomTabNavigator();
 
 const MyTabs = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="myHome" component={HomeScreen} />
-      <Tab.Screen name="Splash" component={Splash} />
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarInactiveTintColor: 'black',
+        tabBarStyle: styles.tabBarStyle,
+        tabBarActiveTintColor: 'red',
+      })}>
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused, color}) => (
+            <View>
+              {focused == true ? (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'row',
+                    padding: 5,
+                    borderRadius: 5,
+                  }}>
+                  <Image
+                    source={require('../Images/Home.png')}
+                    resizeMode="contain"
+                    style={{
+                      height: 30,
+                      width: 30,
+                      tintColor: focused ? Color.mainColor : Color.textColor,
+                    }}
+                  />
+                </View>
+              ) : (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'row',
+                  }}>
+                  <Image
+                    source={require('../Images/Home.png')}
+                    resizeMode="contain"
+                    style={{
+                      height: 30,
+                      width: 30,
+                      tintColor: focused ? Color.mainColor : Color.textColor,
+                    }}
+                  />
+                </View>
+              )}
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="History"
+        component={History}
+        options={{
+          tabBarIcon: ({focused, color}) => (
+            <View>
+              {focused == true ? (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'row',
+                    padding: 5,
+                    borderRadius: 5,
+                  }}>
+                  <Image
+                    source={require('../Images/History.png')}
+                    resizeMode="contain"
+                    style={{
+                      height: 30,
+                      width: 30,
+                      tintColor: focused ? Color.mainColor : Color.textColor,
+                    }}
+                  />
+                </View>
+              ) : (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'row',
+                  }}>
+                  <Image
+                    source={require('../Images/History.png')}
+                    resizeMode="contain"
+                    style={{
+                      height: 30,
+                      width: 30,
+                      tintColor: focused ? Color.mainColor : Color.textColor,
+                    }}
+                  />
+                </View>
+              )}
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Feedback"
+        component={Feedback}
+        options={{
+          tabBarIcon: ({focused, color}) => (
+            <View>
+              {focused == true ? (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'row',
+                    padding: 5,
+                    borderRadius: 5,
+                  }}>
+                  <Image
+                    source={require('../Images/feedback.png')}
+                    resizeMode="contain"
+                    style={{
+                      height: 30,
+                      width: 30,
+                      tintColor: focused ? Color.mainColor : Color.textColor,
+                    }}
+                  />
+                </View>
+              ) : (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'row',
+                  }}>
+                  <Image
+                    source={require('../Images/feedback.png')}
+                    resizeMode="contain"
+                    style={{
+                      height: 30,
+                      width: 30,
+                      tintColor: focused ? Color.mainColor : Color.textColor,
+                    }}
+                  />
+                </View>
+              )}
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="LoginUserProfile"
+        component={LoginUserProfile}
+        options={{
+          tabBarIcon: ({focused, color}) => (
+            <View>
+              {focused == true ? (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'row',
+                    padding: 5,
+                    borderRadius: 5,
+                  }}>
+                  <Image
+                    source={require('../Images/profile.png')}
+                    resizeMode="contain"
+                    style={{
+                      height: 30,
+                      width: 30,
+                      tintColor: focused ? Color.mainColor : Color.textColor,
+                    }}
+                  />
+                </View>
+              ) : (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    flexDirection: 'row',
+                  }}>
+                  <Image
+                    source={require('../Images/profile.png')}
+                    resizeMode="contain"
+                    style={{
+                      height: 30,
+                      width: 30,
+                      tintColor: focused ? Color.mainColor : Color.textColor,
+                    }}
+                  />
+                </View>
+              )}
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
 
 export default MyTabs;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    // position: 'absolute',
+    borderTopWidth: 0,
+    height: 60,
+    borderTopEndRadius: 30,
+    borderTopStartRadius: 30,
+  },
+});
---
+++ Screens/LoginUserProfile/index.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const LoginUserProfile = () => {
+  return (
+    <View>
+      <Text>LoginUserProfile</Text>
+    </View>
+  );
+};
+
+export default LoginUserProfile;
+
+const styles = StyleSheet.create({});
