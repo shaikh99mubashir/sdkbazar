965d47564401b1c4b4412b5a180355e49ab6af48
shaikh99mubashir <shaikh99mubashir@gmail.com>
2023-04-27 17:02:08 +0500

    RestaurantDetails

--- Screens/Foods/RestaurantDetails/index.tsx
+++ Screens/Foods/RestaurantDetails/index.tsx
@@ -43,7 +43,7 @@ const RestaurantDetails = ({navigation, route}: any) => {
     {
       id: 3,
       Foodtype: 'pizza',
-      foodName: 'sss',
+      foodName: 'aaa',
       price: '500',
       raiting: '4.8',
       min: '20 mn',
@@ -71,13 +71,22 @@ const RestaurantDetails = ({navigation, route}: any) => {
     const subdata = selectService.filter(
       (data: any) => data.Foodtype === e.Foodtype,
     );
-    setFilterSubCategory(subdata);
+    if (
+      filterSubCategory &&
+      filterSubCategory[0].Foodtype == subdata[0].Foodtype
+    ) {
+      setFilterSubCategory('');
+      setSelectedSubCategory('');
+    } else {
+      setFilterSubCategory(subdata);
+    }
   };
+
   const [foundName, setFoundName] = useState([]);
   const searchName = (e: any) => {
     setSearchText(e);
     let filteredItems: any = selectService.filter((x: any) =>
-      x.Foodtype.toLowerCase().includes(e.toLowerCase()),
+      x.foodName.toLowerCase().includes(e.toLowerCase()),
     );
     setFoundName(filteredItems);
   };
@@ -189,8 +198,9 @@ const RestaurantDetails = ({navigation, route}: any) => {
           width: '100%',
           marginHorizontal: 10,
         }}>
-        {filterSubCategory && filterSubCategory.length > 0
-          ? filterSubCategory.map((e: any, i: number) => {
+        {searchText && filterSubCategory && foundName.length > 0 ? (
+          foundName.map((e: any, i: number) => {
+            if (e.foodName.toLowerCase().includes(searchText.toLowerCase())) {
               return (
                 <TouchableOpacity
                   activeOpacity={0.8}
@@ -274,185 +284,291 @@ const RestaurantDetails = ({navigation, route}: any) => {
                   </View>
                 </TouchableOpacity>
               );
-            })
-          : foundName && foundName.length > 0
-          ? foundName.map((e: any, i: number) => {
-              return (
-                <TouchableOpacity
-                  activeOpacity={0.8}
-                  key={i}
+            }
+          })
+        ) : searchText && foundName && foundName.length > 0 ? (
+          foundName.map((e: any, i: number) => {
+            return (
+              <TouchableOpacity
+                activeOpacity={0.8}
+                key={i}
+                style={{
+                  height: 200,
+                  backgroundColor: Color.lightgrey,
+                  width: '45%',
+                  borderRadius: 10,
+                  shadowRadius: 10,
+                  elevation: 5,
+                  margin: 5,
+                }}>
+                <Image
+                  source={e.image}
+                  resizeMode="cover"
                   style={{
-                    height: 200,
-                    backgroundColor: Color.lightgrey,
-                    width: '45%',
-                    borderRadius: 10,
-                    shadowRadius: 10,
-                    elevation: 5,
-                    margin: 5,
+                    width: 120,
+                    height: 100,
+                    alignSelf: 'center',
+                    marginTop: 10,
+                  }}
+                />
+                <Text
+                  style={{
+                    paddingHorizontal: 10,
+                    fontSize: 16,
+                    color: Color.mainColor,
+                    fontFamily: 'Poppins-SemiBold',
                   }}>
-                  <Image
-                    source={e.image}
-                    resizeMode="cover"
-                    style={{
-                      width: 120,
-                      height: 100,
-                      alignSelf: 'center',
-                      marginTop: 10,
-                    }}
-                  />
+                  {e.foodName}
+                </Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    gap: 10,
+                    paddingHorizontal: 10,
+                  }}>
                   <Text
                     style={{
-                      paddingHorizontal: 10,
-                      fontSize: 16,
-                      color: Color.mainColor,
+                      fontSize: 13,
+                      color: Color.textColor,
                       fontFamily: 'Poppins-SemiBold',
                     }}>
-                    {e.foodName}
+                    20 min
                   </Text>
                   <View
                     style={{
                       flexDirection: 'row',
-                      gap: 10,
-                      paddingHorizontal: 10,
+                      gap: 5,
+                      alignItems: 'center',
                     }}>
+                    <FontAwesome name="star" size={15} color="gold" />
                     <Text
                       style={{
                         fontSize: 13,
-                        color: Color.textColor,
-                        fontFamily: 'Poppins-SemiBold',
-                      }}>
-                      20 min
-                    </Text>
-                    <View
-                      style={{
-                        flexDirection: 'row',
-                        gap: 5,
-                        alignItems: 'center',
-                      }}>
-                      <FontAwesome name="star" size={15} color="gold" />
-                      <Text
-                        style={{
-                          fontSize: 13,
-                          color: Color.mainColor,
-                          fontFamily: 'Poppins-SemiBold',
-                        }}>
-                        4.2
-                      </Text>
-                    </View>
-                  </View>
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      justifyContent: 'space-between',
-                      paddingHorizontal: 10,
-                    }}>
-                    <Text
-                      style={{
-                        fontSize: 16,
                         color: Color.mainColor,
                         fontFamily: 'Poppins-SemiBold',
                       }}>
-                      Rs: 5454
+                      4.2
                     </Text>
-                    <TouchableOpacity activeOpacity={0.8}>
-                      <AntDesign name="shoppingcart" size={25} color="purple" />
-                    </TouchableOpacity>
                   </View>
-                </TouchableOpacity>
-              );
-            })
-          : selectService &&
-            selectService.length > 0 &&
-            selectService.map((e: any, i: number) => {
-              return (
-                <TouchableOpacity
-                  activeOpacity={0.8}
-                  key={i}
+                </View>
+                <View
                   style={{
-                    height: 200,
-                    backgroundColor: Color.lightgrey,
-                    width: '45%',
-                    borderRadius: 10,
-                    shadowRadius: 10,
-                    elevation: 5,
-                    margin: 5,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingHorizontal: 10,
                   }}>
-                  <Image
-                    source={e.image}
-                    resizeMode="cover"
-                    style={{
-                      width: 120,
-                      height: 100,
-                      alignSelf: 'center',
-                      marginTop: 10,
-                    }}
-                  />
                   <Text
                     style={{
-                      paddingHorizontal: 10,
                       fontSize: 16,
                       color: Color.mainColor,
                       fontFamily: 'Poppins-SemiBold',
                     }}>
-                    {e.foodName}
+                    Rs: 5454
+                  </Text>
+                  <TouchableOpacity activeOpacity={0.8}>
+                    <AntDesign name="shoppingcart" size={25} color="purple" />
+                  </TouchableOpacity>
+                </View>
+              </TouchableOpacity>
+            );
+          })
+        ) : searchText && foundName.length == 0 ? (
+          <View
+            style={{
+              width: '100%',
+              flexDirection: 'row',
+              display: 'flex',
+              justifyContent: 'center',
+            }}>
+            <Text
+              style={{
+                color: Color.textColor,
+                textAlign: 'center',
+                fontSize: 24,
+                fontFamily: 'Poppins-Regular',
+              }}>
+              Search Data Not Found
+            </Text>
+          </View>
+        ) : filterSubCategory && filterSubCategory.length > 0 ? (
+          filterSubCategory.map((e: any, i: number) => {
+            return (
+              <TouchableOpacity
+                activeOpacity={0.8}
+                key={i}
+                style={{
+                  height: 200,
+                  backgroundColor: Color.lightgrey,
+                  width: '45%',
+                  borderRadius: 10,
+                  shadowRadius: 10,
+                  elevation: 5,
+                  margin: 5,
+                }}>
+                <Image
+                  source={e.image}
+                  resizeMode="cover"
+                  style={{
+                    width: 120,
+                    height: 100,
+                    alignSelf: 'center',
+                    marginTop: 10,
+                  }}
+                />
+                <Text
+                  style={{
+                    paddingHorizontal: 10,
+                    fontSize: 16,
+                    color: Color.mainColor,
+                    fontFamily: 'Poppins-SemiBold',
+                  }}>
+                  {e.foodName}
+                </Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    gap: 10,
+                    paddingHorizontal: 10,
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      color: Color.textColor,
+                      fontFamily: 'Poppins-SemiBold',
+                    }}>
+                    20 min
                   </Text>
                   <View
                     style={{
                       flexDirection: 'row',
-                      gap: 10,
-                      paddingHorizontal: 10,
+                      gap: 5,
+                      alignItems: 'center',
                     }}>
+                    <FontAwesome name="star" size={15} color="gold" />
                     <Text
                       style={{
                         fontSize: 13,
-                        color: Color.textColor,
-                        fontFamily: 'Poppins-SemiBold',
-                      }}>
-                      20 min
-                    </Text>
-                    <View
-                      style={{
-                        flexDirection: 'row',
-                        gap: 5,
-                        alignItems: 'center',
-                      }}>
-                      <FontAwesome name="star" size={15} color="gold" />
-                      <Text
-                        style={{
-                          fontSize: 13,
-                          color: Color.mainColor,
-                          fontFamily: 'Poppins-SemiBold',
-                        }}>
-                        4.2
-                      </Text>
-                    </View>
-                  </View>
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      justifyContent: 'space-between',
-                      paddingHorizontal: 10,
-                    }}>
-                    <Text
-                      style={{
-                        fontSize: 16,
                         color: Color.mainColor,
                         fontFamily: 'Poppins-SemiBold',
                       }}>
-                      Rs: 5454
+                      4.2
                     </Text>
-                    <TouchableOpacity activeOpacity={0.8}>
-                      <AntDesign name="shoppingcart" size={25} color="purple" />
-                    </TouchableOpacity>
                   </View>
-                </TouchableOpacity>
-              );
-            })}
+                </View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingHorizontal: 10,
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      color: Color.mainColor,
+                      fontFamily: 'Poppins-SemiBold',
+                    }}>
+                    Rs: 5454
+                  </Text>
+                  <TouchableOpacity activeOpacity={0.8}>
+                    <AntDesign name="shoppingcart" size={25} color="purple" />
+                  </TouchableOpacity>
+                </View>
+              </TouchableOpacity>
+            );
+          })
+        ) : (
+          selectService &&
+          selectService.length > 0 &&
+          selectService.map((e: any, i: number) => {
+            return (
+              <TouchableOpacity
+                activeOpacity={0.8}
+                key={i}
+                style={{
+                  height: 200,
+                  backgroundColor: Color.lightgrey,
+                  width: '45%',
+                  borderRadius: 10,
+                  shadowRadius: 10,
+                  elevation: 5,
+                  margin: 5,
+                }}>
+                <Image
+                  source={e.image}
+                  resizeMode="cover"
+                  style={{
+                    width: 120,
+                    height: 100,
+                    alignSelf: 'center',
+                    marginTop: 10,
+                  }}
+                />
+                <Text
+                  style={{
+                    paddingHorizontal: 10,
+                    fontSize: 16,
+                    color: Color.mainColor,
+                    fontFamily: 'Poppins-SemiBold',
+                  }}>
+                  {e.foodName}
+                </Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    gap: 10,
+                    paddingHorizontal: 10,
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      color: Color.textColor,
+                      fontFamily: 'Poppins-SemiBold',
+                    }}>
+                    20 min
+                  </Text>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      gap: 5,
+                      alignItems: 'center',
+                    }}>
+                    <FontAwesome name="star" size={15} color="gold" />
+                    <Text
+                      style={{
+                        fontSize: 13,
+                        color: Color.mainColor,
+                        fontFamily: 'Poppins-SemiBold',
+                      }}>
+                      4.2
+                    </Text>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingHorizontal: 10,
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      color: Color.mainColor,
+                      fontFamily: 'Poppins-SemiBold',
+                    }}>
+                    Rs: 5454
+                  </Text>
+                  <TouchableOpacity activeOpacity={0.8}>
+                    <AntDesign name="shoppingcart" size={25} color="purple" />
+                  </TouchableOpacity>
+                </View>
+              </TouchableOpacity>
+            );
+          })
+        )}
       </View>
     </ScrollView>
   );
 };
-
 export default RestaurantDetails;
 
 const styles = StyleSheet.create({
